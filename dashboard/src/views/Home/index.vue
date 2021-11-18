a2ce8bdf6872299af4b5f4bb31503e657cbbeb1a
Jonas Castro <jonascastro128@gmail.com>
2021-11-18 08:16:29 -0300

    first commit

---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-brand-main font-black">Home</h1>
+  <h2 class="text-brand-main font-black">Home</h2>
+  <p class="text-brand-main font-black">Home</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
